50ebcf4ea7e19149b8bdf14219c9ec20a7d2d603
clevertoucan@gmail.com <OFwuguConner2921>
2017-01-31 00:03:27 -0500

    first commit

---
+++ discordbot/src/main/java/BotRunner.java
@@ -0,0 +1,27 @@
+import model.ListenerImpl;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * Created by Joshua Owens on 1/30/2017.
+ */
+public class BotRunner {
+
+    public static void main(String[] args){
+        try{
+            JDA jda = new JDABuilder(AccountType.BOT).setToken("MjM2NTgzNTQ3MDkwMTA4NDI3.C3Gliw.d9LaQFStsXF7O0MTYxlnKyUs6Dc").buildBlocking();
+            jda.addEventListener(new ListenerImpl());
+        } catch(LoginException e){
+            e.printStackTrace();
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        } catch(RateLimitedException e){
+            e.printStackTrace();
+        }
+
+    }
+}
